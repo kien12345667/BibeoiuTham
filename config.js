9d36c487b989d4530ea7ffecadb9533f0880fc91
kien12345667 <86776114+kien12345667@users.noreply.github.com>
2021-07-01 18:48:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    introTitle: 'Thắm béo ơiiii',
+    introDesc: `Lần đầu tiên anh bị sét đánh 
+                Đó là lần đầu gặp emm`,
+    btnIntro: 'Iuiuu',
+    title: 'Bi hỏi em đúng 1 câu thôiiii 😙',
+    desc: 'Em có iu Bi khônggggg ',
+    btnYes: 'Em iu Bi nhiềuuu <33',
+    btnNo: 'Không, có cái nịt :3',
+    question:'Xin hỏi thêm 1 câu tại sao em lại chọn anhh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    reply: 'Xin thú thực là Bi đẹp trai quá không chọn không đượcc <3',
+    mess: 'Bi biết mà 🥰. Bi cũng yêu em nhiều lawmsmmm 😘😘',
+    messDesc: 'Chủ nhật này Bi qua với em nhéeee.',
+    btnAccept: 'Chốt đơnnnn <3',
+    messLink: 'https://www.facebook.com/ptkien.2002/' 
 }
